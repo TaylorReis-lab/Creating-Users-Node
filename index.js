3ec5622b7307fbd672dc81e1ef4dde189847d388
TaylorReis-lab <reist98822@gmail.com>
2024-01-21 16:27:35 -0300

    update error

--- index.js
+++ index.js
@@ -1,60 +1,60 @@
-const express = require("express")
-const uuid = require("uuid")
-const cors = require("cors")
-const port = 3001
-const app = express()
-app.use(express.json())
-app.use(cors())
+const express = require("express");
+const uuid = require("uuid");
+const cors = require("cors");
+const port = 3001;
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-const users = []    
+const users = [];
 
 const checkUserId = (request, response, next) => {
-    const {id} = request.params
-    const index = users.findIndex(user => user.id === id)
-    
-    if(index < 0){
-        return response.status(404).json({Error: "user not fund"})
-    }
+  const { id } = request.params;
+  const index = users.findIndex((user) => user.id === id);
 
-    request.userIndexId = index
-    request.userId = id
+  if (index < 0) {
+    return response.status(404).json({ Error: "user not fund" });
+  }
 
-    next
-}
+  request.userIndex = index;
+  request.userId = id;
+
+  next();
+};
 
 app.get("/users", (request, response) => {
-    return response.json(users)
-})
+  return response.json(users);
+});
 
 app.post("/users", (request, response) => {
-    const { name, age } = request.body
-    const user = { id:uuid.v4(), name, age}
+  const { name, age } = request.body;
+  const user = { id: uuid.v4(), name, age };
 
-    users.push(user)
-    return response.status(201).json(user)
-})
+  users.push(user);
+  return response.status(201).json(user);
+});
 
 app.put("/users/:id", checkUserId, (request, response) => {
-    const {name, age} = request.body
-    const index = request.userIndexId
-    const id = request.userId
+  const { name, age } = request.body;
+  const index = request.userIndex;
+  const id = request.userId;
 
-    const updateUser = {id, name, age}
+  const updateUser = { id, name, age };
 
-    users[index] = updateUser
+  users[index] = updateUser;
 
-    return response.json(updateUser)
-})
+  return response.json(updateUser);
+});
 
-app.delete("/users/:id", checkUserId,(request, response) => {
-    const index = request.userIndexId
+app.delete("/users/:id", checkUserId, (request, response) => {
+    const index = request.userIndex;
 
-    users.splice(index, 1)
+    users.splice(index, 1);
 
-    return response.status(204).json()
-})
+    return response.status(204).json();
+});
 
 
 app.listen(port, () => {
-    console.log(`Server Started in port: ${port} 🤞`)
-})
+  console.log(`🚀 Server Started in port: ${port}`);
+});
